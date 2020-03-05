6cbe248f6d55b2bcc2c8aa71e75297c6d14691ee
sh-ikeda <togotv_dell1@DESKTOP-96BPKRE.localdomain>
2020-03-05 15:41:21 +0900

    minor fix

--- metasra_postprocess.sh
+++ metasra_postprocess.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 cat $@ | jq -r '.[] | {accession, m: ."mapped ontology terms"[]} |[.accession, .m.original_key, .m.original_value, .m.term_id, .m.term_name] | @tsv' | sort | awk -f /app/cvcl2xrefs.awk /app/cvcl_xref.tsv -
